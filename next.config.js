72f5b876a90f541507e6c882d89dc207ca067dc5
edegp <a.yuhi1164@gmail.com>
2023-02-09 00:12:13 +0900

    feat: nest css, extarnal image and another setting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     // Required:
     appDir: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maps.gstatic.com',
+        port: '',
+        pathname: '/mapfiles/place_api/icons/v1/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'maps.googleapis.com',
+        port: '',
+        pathname: '/maps/api/place/js/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**'
+      }
+    ]
   }
 }
 
