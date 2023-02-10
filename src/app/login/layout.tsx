ac6ad4b169d406b6ecf420c4429c2eb1251ac775
edegp <a.yuhi1164@gmail.com>
2023-02-11 00:55:15 +0900

    feat: add login page

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,15 @@
+import '@/styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <Head>
+        <title>Fooder Login</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <body className="overflow-y-scroll bg-zinc-900">{children}</body>
+    </html>
+  )
+}
