de4b0ad744c68c6fad1a1ba258eaea7e2e9a224c
edegp <a.yuhi1164@gmail.com>
2023-05-27 23:30:17 +0900

    add: recoil toolkit

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,11 +5,25 @@ import Head from 'next/head'
 
 import { withUrqlClient } from 'next-urql'
 import { RecoilRoot } from 'recoil'
+import { RecoilDevTools } from 'recoil-toolkit'
 
 import { RecoilURLSyncNext } from '@/lib/recoil/RecilURLSyncNext'
 import '@/styles/globals.css'
 import { ErrorBoundary } from '@/ui/atom/ErrorBoundary'
 import { InitialStateProvider } from '@/ui/components/initialStateProvider'
+
+// json 循環参照回避のため
+const getCircularReplacer = () => {
+  const seen = new WeakSet()
+  return (_: string, value: string) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) return
+
+      seen.add(value)
+    }
+    return value
+  }
+}
 
 function Myapp({ Component, pageProps }: AppProps) {
   return (
@@ -21,10 +35,14 @@ function Myapp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <RecoilRoot>
+        <RecoilDevTools
+          forceSerialize={true}
+          serializer={obj => JSON.stringify(obj, getCircularReplacer())}
+        />
         <ErrorBoundary>
           <RecoilURLSyncNext
             location={{ part: 'href' }}
-            serialize={x => JSON.stringify(x)}
+            serialize={x => (typeof x === 'string' ? x : JSON.parse(JSON.stringify(x)))}
             deserialize={x => ({ pathname: x })}
             storeKey="url"
           >
