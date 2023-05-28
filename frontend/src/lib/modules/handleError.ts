392c323bea4da6c718062f6de094c0bad6b93dc4
edegp <a.yuhi1164@gmail.com>
2023-05-28 14:53:40 +0900

    fix: display error message, forget pass layout

--- frontend/src/lib/modules/handleError.ts
+++ frontend/src/lib/modules/handleError.ts
@@ -1,13 +1,28 @@
 import { FirebaseError } from 'firebase/app'
 
 import { isFirebaseError } from '@/lib/firebase'
-import { getJpErrorMessage } from '@/lib/modules/getJpErrorMessage'
+
+const showErrorList = ['EMAIL_NOT_FOUND', 'auth/wrong-password']
+
+type JpErrorMessageObj = {
+  [key: string]: string
+}
+
+const jpErrorMessageObj: JpErrorMessageObj = {
+  EMAIL_NOT_FOUND: 'メールが登録されていません。メールアドレスを確認し，もう一度ログインてください',
+  'auth/email-already-in-use': 'すでに登録済みです。ログイン画面からログインしてください',
+  'auth/user-not-found':
+    'メールが未登録です。メールをもう一度確認するか、新規登録からメールを登録してください。',
+  'auth/wrong-password': 'パスワードが間違っています。もう一度確認してください。'
+}
 
 export const handleError = (error: Error | unknown) => {
   let errorMessage = '通信エラーです。お手数ですが，サポートまでお問い合わせください。'
   if (error instanceof FirebaseError && isFirebaseError(error)) {
-    errorMessage = getJpErrorMessage(error.code)
-    throw new Error(errorMessage)
+    errorMessage =
+      jpErrorMessageObj[error.code] ??
+      '認証エラーです。お手数ですが，サポートまでお問い合わせください。'
+    return errorMessage
   }
   throw new Error('通信エラーです。お手数ですが，サポートまでお問い合わせください。')
 }
